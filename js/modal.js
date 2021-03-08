9dda27bd1ad17da2bb80e7aa219183cd9c7ed0aa
tescofresco <mikeholbrook95@gmail.com>
2021-03-08 19:12:25 +0000

    Work on modal pages using history API

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,8 @@ let modalCont = $('.modal-popup-container');
 let modalBg = $('.modal-background');
 let body = $('body');
 
-function openModal () {
+function openModal(fileString) {
+    modalCont.load(`../projects/${fileString}.html`);
     modalBg.css('opacity', 1);
     modalCont.css('opacity', 0);
     modalBg.css('display', 'block');
@@ -19,8 +20,58 @@ function openModal () {
 }
 
 function closeModal () {
-    modalBg.css('display', 'none');
-    modalCont.css('display', 'none');
-    body.removeClass('modal-open');
+    anime({
+        targets:'.modal-fade',
+        duration:300,
+        opacity:[0,1],
+        easing:"easeInOutCubic",
+        delay:anime.stagger(100),
+        direction:"reverse",
+        complete: function(anim) {
+            modalBg.css('display', 'none');
+            modalCont.css('display', 'none');
+            body.removeClass('modal-open');
+        }
+    });
+    
 
 }
+
+$( document ).ready(function() {
+    //when page loads check if there is a URL hash, if so open the appropriate modal.
+    if(location.hash != "") {
+        let fileString = location.hash;
+        fileString = fileString.replace("#", "");
+        openModal(fileString);
+    }
+
+    //listen for changes to the url hash for browser button modal navigation
+    window.addEventListener("hashchange", hashChange, false);
+
+
+});
+
+function hashChange () {
+    if(location.hash != "") {
+        let fileString = location.hash;
+        fileString = fileString.replace("#", "");
+        openModal(fileString);
+    } else {
+        closeModal();
+    }
+}
+
+
+//sets up onclick for cards on page
+(function setupLinks() {
+    $('.card').each((i, obj) => {
+        $(obj).click( () => {
+            openModal($(obj).attr('data-target'));
+            location.hash = `#${$(obj).attr('data-target')}`;
+        });
+    })
+
+    $('.modal-background').click(() => {
+        location.hash = "";
+    });
+}());
